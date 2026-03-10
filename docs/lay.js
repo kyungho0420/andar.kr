54dec890c7e8e225008310763954e0fc1dfcaf36
kyungho0420 <kyungho0420@gmail.com>
2026-03-10 17:47:28 +0900

    Express V4 규격 리팩토링

--- docs/lay.js
+++ docs/lay.js
@@ -12,7 +12,7 @@ const siteConfig = {
     },
     api: {
         server: 'damso',
-        turnstile: '0x4AAAAAABrG4DQP8tkp1_TI',
+        turnstile: '0x4AAAAAABy-w_A_C6_e0x6H', // Updated for Modal Mode if needed, or keeping current if valid
         redirect: '../'
     },
     canvas: {
